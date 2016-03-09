144ad5557f6b0176632e21a2f89615cf14402722
Russell Osborne <russell@theironyard.com>
2016-03-09 16:15:28 -0500

    Adding array and hash pry sessions

---
+++ week1/03/array_pry_sessions.rb
@@ -0,0 +1,7 @@
+array = [] # => []
+array[5] = ["H", "H"] # => ["H", "H"]
+array[6] # => nil
+array[5] # => ["H", "H"]
+lookuptable = {"2"=> 0, "A" => 11} # => {"2"=>0, "A"=>11}
+lookuptable["A"] # => 11
+hash #
