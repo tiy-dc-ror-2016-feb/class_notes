9f2c32c9877daff4ca097aa90e65b26ab757f682
Russell Osborne <russell@theironyard.com>
2016-03-28 16:08:41 -0400

    adding mock example

---
+++ week4/16/issue.rb
@@ -0,0 +1,29 @@
+require 'net/http'
+require 'json'
+require 'pry'
+
+class Issue
+  ENDPOINT = "https://api.github.com/issues"
+  def list
+    # Turn the request body into a ruby object
+    response = query_list
+
+    return JSON.parse(response)
+  end
+
+  private
+
+  def query_list
+    uri = URI(ENDPOINT)
+    # Build client
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    # Build request
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req.basic_auth(ENV['GITHUB_USER'], ENV['GITHUB_PASSWORD'])
+
+    # Actually Perform the request
+    response = http.request(req).body
+  end
+end
---
+++ week4/16/issue_test.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'json'
+require 'pry'
+require 'minitest/autorun'
+require './issue'
+
+class Issue
+
+  def query_list
+    File.read("./authed_issues.json")
+  end
+
+end
+
+class IssueTest < Minitest::Test
+  def test_issue_can_list_issues
+    api = Issue.new
+    assert_equal 2, api.list.size
+  end
+end
